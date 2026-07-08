995cdfcea2329f52459954ee2f85bb1217b9ce01
Harshit2N <harshitsinghshakya23@gmail.com>
2026-07-09 01:41:30 +0530

    feat: update password hashing functions and increase salt value

--- server/src/lib/password.ts
+++ server/src/lib/password.ts
@@ -1,2 +1,11 @@
 import bcrypt from "bcrypt"
-const salt:number=10;
+
+const salt: number = 12;
+
+const hashPassword = async ({ password }: { password: string }): Promise<string> => {
+    return await bcrypt.hash(password, salt);
+}
+
+const verfiyPassword = async ({ password, hashPassword }: { password: string; hashPassword: string }): Promise<boolean> => {
+    return await bcrypt.compare(password, hashPassword);
+}
